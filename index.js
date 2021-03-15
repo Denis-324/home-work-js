d9dbc7ae5f8e4f7491cdfa5433e19e7567b37ce6
Your Name <you@example.com>
2021-03-15 12:41:50 +0300

    home work

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//1.   "кот" > "код"
+//2.   "2" + 2 * "2"
+//3.   undefined == null
+//4.   undefined === null
+//5.   null == 0
+//6.   2 > "3"
+//7.   null - false + true
+//8.   1 / "l"
+//9.   "2" * "3"
+//10.  4 + 5 + "O"
+//11.  "l" + 4 + 5
+//12.  "4" - 2
+//13.  "4" - "4x"
+//14.  null == false
+//15.  " -4 "/ 0 + 1
+//16.  null + 1
+//17.  undefined + null
+
+
+console.log('№1 ', typeof ("кот" > "код"), "кот" > "код");
+console.log('№2 ', typeof ("2" + 2 * "2"), "2" + 2 * "2");
+console.log('№3 ', typeof (undefined == null), undefined == null);
+console.log('№4 ', typeof (undefined === null), undefined === null);
+console.log('№5 ', typeof (null == 0), null == 0);
+console.log('№6 ', typeof (2 > "3"), 2 > "3");
+console.log('№7 ', typeof (null - false + true), null - false + true);
+console.log('№8 ', typeof (1 / "l"), 1 / "l");
+console.log('№9 ', typeof ("2" * "3"), "2" * "3");
+console.log('№10 ', typeof (4 + 5 + "O"), 4 + 5 + "O");
+console.log('№11 ', typeof ("l" + 4 + 5), "l" + 4 + 5);
+console.log('№12 ', typeof ("4" - 2), "4" - 2);
+console.log('№13 ', typeof ("4" - "4x"), "4" - "4x");
+console.log('№14 ', typeof (null == false), null == false);
+console.log('№15 ', typeof (" -4 " / 0 + 1), " -4 " / 0 + 1);
+console.log('№16 ', typeof (null + 1), null + 1);
+console.log('№17 ', typeof (undefined + null), undefined + null);
