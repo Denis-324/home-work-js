ba6e5b3bf866b68e2eafeadeb36c7a80798c877c
Denis <you@example.com>
2021-05-29 23:37:16 +0300

    todo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let container = document.querySelector(".container");
 let form = document.querySelector(".form");
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
@@ -8,9 +9,24 @@ let listCrosse = document.querySelector(".list-crosse");
 
 let clickBtn = function () {
   let liClone = li.cloneNode(true);
-  liClone.querySelector(".list-text").textContent = input.value;
+  liClone.querySelector(".list-text").innerHTML = input.value;
   list.append(liClone);
   input.value = "";
+  listText.classList.remove("actives");
+  listText.classList.remove("active");
 };
 
 btn.addEventListener("click", clickBtn);
+
+let clicklistCrosse = function () {
+  listText.classList.add("actives");
+  listCrosse.classList.add("actives");
+};
+
+listCrosse.addEventListener("click", clicklistCrosse);
+
+// let clicklistText = function () {
+//   listText.classList.toggle("active");
+// };
+
+// li.addEventListener("click", clicklistText);
